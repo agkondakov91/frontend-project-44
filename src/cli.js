d37fbae34a7fd190ea13ae70888e0f98e60e3101
Alexander Kondakov <agkondakov91@gmail.com>
2025-07-01 17:40:40 +0300

    add readline-sync

---
+++ src/cli.js
@@ -0,0 +1,7 @@
+// Импортируем модуль из библиотеки
+import readline from "readline-sync";
+
+export const getName = () => {
+  const name = readline.question("May I have your name? ");
+  console.log(`Hello, ${name}`);
+};
